f2cc09291e40c0f880972bd8efca72d77dd62f90
Alexander <mrreads@yandex.com>
2020-03-30 15:16:39 +0300

    create task

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
 const boards = document.querySelector('#boards');
 
 let allBoardAdd = document.querySelectorAll('.board-add');
+let allTaskAdd, taskAdd;
 
 allBoardAdd.forEach(boardAdd => 
 {
-    boardAdd.addEventListener('click', (event) => 
+    boardAdd.addEventListener('click', eventBoard => 
     {
-        event.target.insertAdjacentHTML('beforeBegin', 
+        let arr = eventBoard.target.insertAdjacentHTML('beforeBegin', 
         `<div class="board">
         <h1 class="board-name">simple text</h1>
             <p class="task">simple task</p>
             <p class="add">add another task</p>
-        </div>`)
-    })
+        </div>`);
+
+        taskAdd = boardAdd.previousElementSibling.querySelector('.add');
+        taskAdd.addEventListener('click', eventTask =>
+        {
+            eventTask.target.insertAdjacentHTML('beforeBegin', 
+            `<p class="task">simple task</p>`);
+        });
+    });
 });
+
